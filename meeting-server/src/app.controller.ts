7a8bcc7dfe23089ca7beccc83339085ebd69edca
BetzalelKenig <59796236+BetzalelKenig@users.noreply.github.com>
2020-11-24 12:46:49 +0200

    build for heroku

--- meeting-server/src/app.controller.ts
+++ meeting-server/src/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import * as path from 'path';
 
-@Controller()
+@Controller('meeting')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
+
+  // @Get()
+  // getHello(): string {
+  //   return this.appService.getHello();
+  // }
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  root(@Res() response): void {
+    // the homepage will load our index.html which contains angular logic
+    response.sendFile(__dirname + '/public/index.html');
   }
 }
--- meeting-server/src/main.ts
+++ meeting-server/src/main.ts
@@ -2,8 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const port = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
